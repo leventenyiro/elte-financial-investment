a8f4eff548b5f35625073dce83cb3c65a60f15f6
bence <uf8gh9@inf.elte.hu>
2023-12-03 20:18:51 +0100

    add cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,18 @@
 import express from "express";
 import userRouter from "./routes/user_router.js";
 import sequelizeDb from "./database/db.js";
-
+import cors from "cors";
 await sequelizeDb.sync();
 
 const app = express();
 const port = 4000;
 
-app.use(express.json());
+var corsOptions = {
+  origin: "http://localhost:3000",
+  optionsSuccessStatus: 200,
+};
+
+app.use(express.json(), cors(corsOptions));
 
 app.use("/user", userRouter);
 
