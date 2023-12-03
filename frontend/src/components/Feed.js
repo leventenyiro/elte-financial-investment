e52bfb169219f207f1755db919a69c7b7d1f65b1
leventenyiro <nyiro.levente@gmail.com>
2023-12-03 18:26:48 +0100

    #19: add feed

---
+++ frontend/src/components/Feed.js
@@ -0,0 +1,39 @@
+function Feed() {
+    return (
+        <div className="Feed container">
+            <h1>News for you</h1>
+            <div className="row">
+                <div className='col-md-4 pt-4'>
+                    <div className='news'>
+                        <img src='https://img.freepik.com/free-vector/gradient-stock-market-concept_23-2149166910.jpg' className='img-fluid' />
+                        <h2 className='px-4 pt-4'>Tech Stocks Propel NASDAQ to Record Highs</h2>
+                        <p className='px-4 py-3'>In a surprising turn of events, major tech stocks experienced a sudden surge, driving the NASDAQ to a record high. Analysts attribute this unexpected boost to positive quarterly earnings reports and growing investor confidence in the sector.</p>
+                    </div>
+                </div>
+                <div className='col-md-4 pt-4'>
+                    <div className='news'>
+                        <img src='https://img.freepik.com/free-vector/gradient-stock-market-concept_23-2149166910.jpg' className='img-fluid' />
+                        <h2 className='px-4 pt-4'>Tech Stocks Propel NASDAQ to Record Highs</h2>
+                        <p className='px-4 py-3'>In a surprising turn of events, major tech stocks experienced a sudden surge, driving the NASDAQ to a record high. Analysts attribute this unexpected boost to positive quarterly earnings reports and growing investor confidence in the sector.</p>
+                    </div>
+                </div>
+                <div className='col-md-4 pt-4'>
+                    <div className='news'>
+                        <img src='https://img.freepik.com/free-vector/gradient-stock-market-concept_23-2149166910.jpg' className='img-fluid' />
+                        <h2 className='px-4 pt-4'>Tech Stocks Propel NASDAQ to Record Highs</h2>
+                        <p className='px-4 py-3'>In a surprising turn of events, major tech stocks experienced a sudden surge, driving the NASDAQ to a record high. Analysts attribute this unexpected boost to positive quarterly earnings reports and growing investor confidence in the sector.</p>
+                    </div>
+                </div>
+                <div className='col-md-4 pt-4'>
+                    <div className='news'>
+                        <img src='https://img.freepik.com/free-vector/gradient-stock-market-concept_23-2149166910.jpg' className='img-fluid' />
+                        <h2 className='px-4 pt-4'>Tech Stocks Propel NASDAQ to Record Highs</h2>
+                        <p className='px-4 py-3'>In a surprising turn of events, major tech stocks experienced a sudden surge, driving the NASDAQ to a record high. Analysts attribute this unexpected boost to positive quarterly earnings reports and growing investor confidence in the sector.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Feed;
