e785c9f724b0e2c2d96499bd7b68b342c3207025
vago-blanka <vago.blanka@leafsoft.hu>
2023-12-10 13:07:07 +0100

    create and import investment router

---
+++ backend/src/routes/investments_router.js
@@ -0,0 +1,33 @@
+import InvestmentsController from "../controllers/investments_controller.js";
+import InvestmentValidator from "../validators/investment_validator.js";
+import authGuard from "../middlewares/auth_guard.js";
+import errorHandler from "../middlewares/error_handler.js";
+import express from "express";
+
+const investmentRouter = express.Router();
+const investmentController = new InvestmentsController();
+const investmentValidator = new InvestmentValidator();
+
+investmentRouter.get(
+  "/risk/:risk",
+  authGuard,
+  investmentValidator.checkRisk(),
+  errorHandler,
+  investmentController.getRisk
+);
+investmentRouter.get(
+  "/topic/:topic",
+  authGuard,
+  investmentValidator.checkTopic(),
+  errorHandler,
+  investmentController.getTopic
+);
+investmentRouter.get(
+  "/weekly",
+  authGuard,
+  investmentValidator.checkDate(),
+  errorHandler,
+  investmentController.getWeekly
+);
+
+export default investmentRouter;
