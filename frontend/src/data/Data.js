4b4174e1e646bca58e888709adbaa95b8fcacb8c
leventenyiro <nyiro.levente@gmail.com>
2023-12-17 17:45:40 +0100

    modify endpoint in Data.js

--- frontend/src/data/Data.js
+++ frontend/src/data/Data.js
@@ -1,7 +1,7 @@
 import { getCookie } from "../utils/getCookie";
 class Data {
-  // static url = "https://elte-financial-investment-rest.azurewebsites.net";
-  static url = "http://localhost:4000";
+  static url = "https://elte-financial-investment-rest.azurewebsites.net";
+  // static url = "http://localhost:4000";
 
   static authCookieValue = document.cookie.includes("authCookie=")
     ? document.cookie
